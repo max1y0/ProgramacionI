8886315fedf249c9be7e57d32bdb495bb55844c4
Maxi Chaves <maximiliano.e.chaves@gmail.com>
2020-09-02 12:40:09 -0300

    agrego ejercicio tipo triangulos

---
+++ triangulo.py
@@ -0,0 +1,19 @@
+#Completar la condicion lógica en la linea 10 y 13 SOLAMENTE
+
+
+#cargamos las tres variables
+lado1 = int(input("Ingresá el lado 1 del triangulo"))
+lado2 = int(input("Ingresá el lado 2 del triangulo"))
+lado3 = int(input("Ingresá el lado 3 del triangulo"))
+
+#primera condicion pregunta si es equilatero
+if (     ):
+  print("tu triangulo es equilátero")
+#segunda condicion va a preguntar si es escaleno
+elif (       ):
+  print("tu triangulo es escaleno")
+#en la tercera condicion voy a usar un ELSE. 
+#ya que si no es equilatero ni escaleno, si o si es isosceles.
+#así evito escribir la condicion lógica más dificil
+else:
+  print ("tu triangulo es isosceles")
